2c08d11201bbff597632dcd1ad344ba1993e8c57
Tal Levy <tall@wix.com>
2019-10-19 15:47:23 +0300

    ad matay

--- wordToVector.py
+++ wordToVector.py
@@ -6,36 +6,35 @@ import warnings
 from nltk.corpus import stopwords
 import string
 import nltk
-
-
 warnings.filterwarnings(action='ignore')
-
 import gensim
-from gensim.models import Word2Vec
 
 #  Reads ‘alice.txt’ file
-sample = open("./Dataset/lyrics1.csv", "r")
-s = sample.read()
+sample = open("./Dataset/lyrics15LIN.csv", "r")
+f = sample.read()
 
 # Replaces escape character with space
-f = s.replace("\n", " ")
 stop = set(stopwords.words('english'))
 stop = stop | (set(stopwords.words('spanish')))
-freq = nltk.FreqDist(f)
-stop = stop | set(freq.keys())
-
+# freq = nltk.FreqDist(f)
+# stop = stop | set(freq.keys())
 data = []
-
 # iterate through each sentence in the file
 for i in sent_tokenize(f):
     temp = []
     # tokenize the sentence into words
     for j in word_tokenize(i):
-        if(j.isalpha() and (j.lower() not in stop) and len(j) > 1 ):
-         temp.append(j.lower())
+        if(j.isalpha() and (j.lower() not in stop) and len(j) > 1):
+            temp.append(j.lower())
+    print(temp)
     data.append(temp)
 # Create Skip Gram model
 
-model2 = gensim.models.Word2Vec(data, min_count=1, size=300,
+model = gensim.models.Word2Vec(data, min_count=1, size=300,
                                 window=5, sg=1)
-print(model2.similarity('distancia', 'funciona'))
+print(model.similarity('distancia', 'something'))
+
+b = (model['king'] - model['man'] + model['woman'])
+print(model.similar_by_vector(b))
+b = (model['man'] - model['money'] - model['home'])
+print(model.similar_by_vector(b))
